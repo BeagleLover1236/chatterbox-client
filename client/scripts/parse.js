58e5fb88f7ff5217a380408fd3a61599ff5d30a4
unoriginal1234 <eric.t.roth@gmail.com>
2024-02-02 18:23:54 -0800

    Day one progress in client/scripts/parse.js

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -8,7 +8,27 @@ var Parse = {
 
   create: function(message, successCB, errorCB = null) {
     // TODO: send a request to the Parse API to save the message
+    // THIS WOULD BE THE POST
+    $.ajax({
+      url: Parse.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+      },
+    })
   },
+
+  // var message = {
+  //   username: 'shawndrost',
+  //   text: 'trololo',
+  //   roomname: '4chan',
+  // };
 
   readAll: function(successCB, errorCB = null) {
     $.ajax({
